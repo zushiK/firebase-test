7195caa35f71c4f7b1b8a489e47a3d3865e7d98d
zushi <k.kawamura@sai.co.jp>
2020-07-01 15:34:07 +0900

    commit : add lightbox and firabse config

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <ul>
+      <li v-for="(img,index) in images" :key='index' @click="show(index)">
+        <img :src="img.thumb">
+      </li>
+    </ul>
+    <ImageLightBox
+      ref="lightBox"
+      :media="images"
+      :show-light-box="true"
+      :show-caption="true"></ImageLightBox>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageLightBox from 'vue-image-lightbox';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+      ImageLightBox
+  },
+  data: function(){
+    return {
+      images: [
+        {
+          thumb: 'https://dummyimage.com/150x150/fcc/000.png&text=thumb1',
+          src: 'https://dummyimage.com/600x600/fcc/000.png&text=Uploaded1',
+          caption: 'キャプション1',
+          width: 800, // required
+          height: 600, // required
+        },
+        {
+          thumb: 'https://dummyimage.com/150x150/fcc/000.png&text=thumb2',
+          src: 'https://dummyimage.com/600x600/fcc/000.png&text=Uploaded2',
+          caption: 'キャプション2',
+          width: 800, // required
+          height: 600, // required
+        },
+        {
+          thumb: 'https://dummyimage.com/150x150/fcc/000.png&text=thumb3',
+          src: 'https://dummyimage.com/600x600/fcc/000.png&text=Uploaded3',
+          caption: 'キャプション3',
+          width: 800, // required
+          height: 600, // required
+        }
+      ]
+    }
+  },
+  mounted: function () {
+    console.log(this.$refs.lightBox)
+  },
+  methods: {
+    show: function(index){
+      console.log(this.$refs,index);
+      this.$refs.lightBox.showImage(index);
+    }
+  },
 }
 </script>
 
@@ -24,5 +69,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  padding: 20px;
+  font-family: Helvetica;
+}
+
+#app {
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  transition: all 0.2s;
+}
+
+li {
+  display:inline-block;
+  border:1px solid #ddd;
+  margin: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueLazyLoad from 'vue-lazyload'
+// import LightBox from 'vue-image-lightbox'
+require('vue-image-lightbox/dist/vue-image-lightbox.min.css');
 Vue.config.productionTip = false
+Vue.use(VueLazyLoad);
 
 new Vue({
   render: h => h(App),
